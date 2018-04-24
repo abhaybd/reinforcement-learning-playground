84e8ebf3c292b97d5ceddb3f9c03237f8bc4bade
Abhay D <a.deshpande012@gmail.com>
2018-04-23 18:02:14 -0700

    Created Organism class to hold data

--- utils.py
+++ utils.py
@@ -5,14 +5,17 @@ import struct
 MUTATE_SPREAD = 2.0
 MUTATE_SHUFFLE_MAX_SIZE = 4
 
-def model_to_arr(model):
+def model_to_organism(model):
       values = [K.get_value(w) for w in model.weights]
       shapes = [arr.shape for arr in values]
       values = np.hstack((arr.flatten() for arr in values))
-      return values, shapes
+      organism = Organism(values, shapes)
+      return organism
 
-def model_from_arr(model, shapes, flattened):
-      def product(*nums):
+def organism_to_model(model, organism):
+      shapes = organism.shapes
+      genome = organism.genome
+      def product(nums):
           p = 1
           for i in nums:
               p *= i
@@ -20,19 +23,22 @@ def model_from_arr(model, shapes, flattened):
       index = 0
       values = []
       for shape in shapes:
-            num_values = product(*shape)
-            values.append(np.array(flattened[index:index+num_values]).reshape(shape))
+            num_values = product(shape)
+            values.append(np.array(genome[index:index+num_values]).reshape(shape))
             index += num_values
       for w,v in zip(model.weights, values):
             K.set_value(w,v)
 
-def create_offspring(model1, model2):
-      arr1 = model_to_arr(model1)
-      arr2 = model_to_arr(model2)
-      cross_index = np.random.randint(0,len(arr1))
-      offspring1 = arr1[:cross_index]+arr2[cross_index:]
-      offspring2 = arr2[:cross_index]+arr1[cross_index:]
-      return mutate(offspring1), mutate(offspring2)
+def create_offspring(organism1, organism2):
+      genome1 = organism1.genome
+      genome2 = organism2.genome
+      cross_index = np.random.randint(0,len(genome1))
+      offspring1 = Organism(genome1[:cross_index]+genome2[cross_index:], organism1.shapes)
+      offspring2 = Organism(genome2[:cross_index]+genome1[cross_index:], organism2.shapes)
+      
+      offspring1.mutate()
+      offspring2.mutate()
+      return offspring1, offspring2
 
 def float_to_bits(f):
       s = struct.pack('>f', f)
@@ -40,15 +46,29 @@ def float_to_bits(f):
 
 def bits_to_float(b):
       s = struct.pack('>l', b)
-      return struct.unpack('>f', s)[0]
+      return struct.unpack('>f', s)[0]      
 
-def mutate(genome):
-      for i, gene in enumerate(genome):
-            genome[i] += np.random.normal(loc=gene, scale=MUTATE_SPREAD)
-      if np.random.random() < 1/len(genome): # Should shuffle?
-            start = np.random.randint(len(genome))
-            stop = start + np.random.randint(1,MUTATE_SHUFFLE_MAX_SIZE)
-            stop = np.clip(stop, 0, len(genome)-1)
-            to_shuffle = genome[start:stop]
-            np.random.shuffle(to_shuffle)
-            genome[start:stop] = to_shuffle
+class Organism(object):
+      def from_model(model):
+            return model_to_organism(model)
+      
+      def __init__(self, genome, shapes):
+            self.genome = genome
+            self.shapes = shapes
+      
+      def mutate(self):
+            for i, gene in enumerate(self.genome):
+                  self.genome[i] += np.random.normal(loc=gene, scale=MUTATE_SPREAD)
+            if np.random.random() < 1/len(self.genome): # Should shuffle?
+                  start = np.random.randint(len(self.genome))
+                  stop = start + np.random.randint(1,MUTATE_SHUFFLE_MAX_SIZE)
+                  stop = np.clip(stop, 0, len(self.genome)-1)
+                  to_shuffle = self.genome[start:stop]
+                  np.random.shuffle(to_shuffle)
+                  self.genome[start:stop] = to_shuffle
+      
+      def to_model(self, model):
+            return organism_to_model(model, self)
+      
+      def create_offspring(self, organism):
+            return create_offspring(self, organism)
